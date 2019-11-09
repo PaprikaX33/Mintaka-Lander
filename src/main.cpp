a50f56858668affaafa0642ba9ae45ad22a68cce
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-11-10 03:20:47 +0700

    added basic windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main(int argc, char ** argv)
@@ -6,5 +8,33 @@ int main(int argc, char ** argv)
     std::cout << argv[i] << ' ';
   }
   std::cout << '\n';
+  sf::RenderWindow window{sf::VideoMode(800.0f, 800.0f), "Mintaka", sf::Style::Titlebar | sf::Style::Close};
+  sf::View viewP{sf::FloatRect{0.0f, 0.0f, 800.0f, 800.0f}};
+  while(window.isOpen()){
+    sf::Event winEvent;
+    while(window.pollEvent(winEvent)){
+      switch(winEvent.type){
+      case sf::Event::Closed:
+        window.close();
+        break;
+      case sf::Event::KeyReleased: {
+        switch(winEvent.key.code){
+        case sf::Keyboard::Q:
+        case sf::Keyboard::Escape:
+          window.close();
+          break;
+        default:
+          break;
+        }
+      }
+        break;
+      default:
+        break;
+      }
+    }
+    window.setView(viewP);
+    window.clear(sf::Color::Black);
+    window.display();
+  }
   return 0;
 }
