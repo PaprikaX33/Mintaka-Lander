a343725cc5ec1620fe5437d4527bbafb2b8a419f
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-11-10 04:04:31 +0700

    added font and dummy fps show

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
@@ -10,14 +11,29 @@ int main(int argc, char ** argv)
   std::cout << '\n';
   sf::RenderWindow window{sf::VideoMode(800.0f, 800.0f), "Mintaka", sf::Style::Titlebar | sf::Style::Close};
   sf::View viewP{sf::FloatRect{0.0f, 0.0f, 800.0f, 800.0f}};
+  sf::Text info;
+  sf::Font font;
+  if(!font.loadFromFile("./font/UbuntuMono-B.ttf")){
+    std::cerr << "Unable to open font\n";
+    return -1;
+  }
+  window.setFramerateLimit(60);
+  info.setFont(font);
+  info.setCharacterSize(30);
+  info.setFillColor(sf::Color::Red);
+  info.setPosition(10,10);
+
   while(window.isOpen()){
+    std::stringstream stream;
+    stream << "FPS:\t" << "10" <<'\n';
+    info.setString(stream.str());
     sf::Event winEvent;
     while(window.pollEvent(winEvent)){
       switch(winEvent.type){
       case sf::Event::Closed:
         window.close();
         break;
-      case sf::Event::KeyReleased: {
+      case sf::Event::KeyReleased:{
         switch(winEvent.key.code){
         case sf::Keyboard::Q:
         case sf::Keyboard::Escape:
@@ -26,14 +42,14 @@ int main(int argc, char ** argv)
         default:
           break;
         }
-      }
-        break;
+      }break;
       default:
         break;
       }
     }
     window.setView(viewP);
     window.clear(sf::Color::Black);
+    window.draw(info);
     window.display();
   }
   return 0;
