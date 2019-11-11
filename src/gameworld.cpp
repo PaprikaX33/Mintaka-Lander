e3e50350061c02eeb636e2fc9a6cd9077b1085c0
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-11-11 12:23:12 +0700

    added player entity

--- src/gameworld.cpp
+++ src/gameworld.cpp
@@ -9,15 +9,19 @@
 GameWorld::GameWorld():
   _window{sf::VideoMode(800.0f, 600.0f), "Mintaka", sf::Style::Titlebar | sf::Style::Close},
   _port{sf::FloatRect{0.0f, 0.0f, 800.0f, 800.0f}},
-  _clock{}
+  _clock{},
+  _player{sf::Vector2f{10.0f, 10.0f}}
 {
   _window.setFramerateLimit(60);
+  _player.setPosition(sf::Vector2f{100.0f,100.0f});
+  _player.setFillColor(sf::Color::Black);
 }
 
 int GameWorld::run()
 {
   sf::Text info;
   sf::Font font;
+  sf::Color resetCol{51, 77, 77, 255};
   if(!font.loadFromFile("./font/TheFont.ttf")){
     std::cerr << "Unable to open font\n";
     return -1;
@@ -53,8 +57,9 @@ int GameWorld::run()
       }
     }
     _window.setView(_port);
-    _window.clear(sf::Color::Black);
+    _window.clear(resetCol);
     _window.draw(info);
+    _window.draw(_player);
     _window.display();
   }
   return 1;//TODO
--- src/include/GameWorld.hpp
+++ src/include/GameWorld.hpp
@@ -1,6 +1,7 @@
 #ifndef MINTAKA_GAME_WORLD_HEAD_HPP
 #define MINTAKA_GAME_WORLD_HEAD_HPP
 #include <SFML/Window.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics.hpp>
 
 class GameWorld
@@ -9,6 +10,7 @@ private:
   sf::RenderWindow _window;
   sf::View _port;
   sf::Clock _clock;
+  sf::RectangleShape _player;
 private:
   GameWorld(GameWorld const &) = delete;
   GameWorld(GameWorld &&) = delete;
