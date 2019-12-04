e01244ab0d4800599dd62a8d5fcbae3ab84b0c23
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-12-05 03:33:51 +0700

    added the code to open the file

--- src/ground_parse.cpp
+++ src/ground_parse.cpp
@@ -1,9 +1,19 @@
-#include <ifstream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <string_view>
 #include "Ground.hpp"
 
 
 Ground Ground::generate_file(char const * fname)
 {
-  std::ifstream file{fname};
-
+  std::ifstream file;
+  std::vector<std::size_t> temp;
+  file.exceptions(std::ifstream::badbit);
+  file.open(fname);
+  std::string const data = [&] (void) {
+                               std::ostringstream tmp;
+                               tmp << file.rdbuf();
+                               return tmp.str();} ();
+  return Ground{temp};
 }
