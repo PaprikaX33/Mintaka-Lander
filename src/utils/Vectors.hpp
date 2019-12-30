5e7ad2725f2690b76a48dba05078d446edf69432
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-12-30 19:07:07 +0700

    added the header for vector utilities

---
+++ src/utils/Vectors.hpp
@@ -0,0 +1,12 @@
+#ifndef MINTAKA_UTILS_VECTOR_HEAD_HPP
+#define MINTAKA_UTILS_VECTOR_HEAD_HPP
+#include <tuple>
+#include <SFML/System/Vector2.hpp>
+namespace utls
+{
+  sf::Vector2f normalize(sf::Vector2f const &) noexcept;
+  float projection(sf::Vector2f const & vec, sf::Vector2f const & axis) noexcept;
+}
+
+
+#endif //MINTAKA_UTILS_VECTOR_HEAD_HPP
