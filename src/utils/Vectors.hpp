0a3b3420076418c34c75894a432169ecb6c44c14
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-01-01 05:22:10 +0700

    added the normal finding function
    `normal_right` generates the normal vector pointing to the right of
    the argument vector, while `normal_left` is pointing to the left

--- src/utils/Vectors.hpp
+++ src/utils/Vectors.hpp
@@ -9,6 +9,8 @@ namespace utls
   float length(sf::Vector2f const &) noexcept;
   //AXIS MUST BE NORMALIZED
   float projection(sf::Vector2f const & vec, sf::Vector2f const & axis) noexcept;
+  sf::Vector2f normal_right(sf::Vector2f const &)noexcept;
+  sf::Vector2f normal_left(sf::Vector2f const &)noexcept;
 }
 
 #endif //MINTAKA_UTILS_VECTOR_HEAD_HPP
--- src/vector.cpp
+++ src/vector.cpp
@@ -11,7 +11,16 @@ float utls::length(sf::Vector2f const & in) noexcept
   return std::hypotf(in.x, in.y);
 }
 
-float projection(sf::Vector2f const & vec, sf::Vector2f const & axis) noexcept
+float utls::projection(sf::Vector2f const & vec, sf::Vector2f const & axis) noexcept
 {
   return (vec.x * axis.x) + (vec.y * axis.y);
+}
+
+sf::Vector2f utls::normal_right(sf::Vector2f const & in)noexcept
+{
+  return sf::Vector2f{in.y, (-1.0f) * in.x};
+}
+sf::Vector2f utls::normal_left(sf::Vector2f const & in)noexcept
+{
+  return sf::Vector2f{(-1.0f) * in.y, in.x};
 }
