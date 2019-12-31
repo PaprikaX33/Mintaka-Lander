6587d0f67c63320b1a16c125e82ec5f1c5eb2307
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-01-01 05:06:24 +0700

    normalized the grid manually, to remote the setorigin

--- src/player.cpp
+++ src/player.cpp
@@ -9,17 +9,15 @@ Player::Player(void) :
   _velocity{0.0f, 0.0f},
   _sprite{sf::LineStrip, 5}
 {
-  auto const pos0 = sf::Vector2f{10.0f,  0.0f};
-  auto const pos1 = sf::Vector2f{20.0f, 20.0f};
-  auto const pos2 = sf::Vector2f{10.0f, 15.0f};
-  auto const pos3 = sf::Vector2f{ 0.0f, 20.0f};
-  auto const pos4 = sf::Vector2f{10.0f,  0.0f};
+  auto const pos0 = sf::Vector2f{  0.0f, -10.0f};
+  auto const pos1 = sf::Vector2f{ 10.0f,  10.0f};
+  auto const pos2 = sf::Vector2f{  0.0f,   5.0f};
+  auto const pos3 = sf::Vector2f{-10.0f,  10.0f};
   _sprite[0] = sf::Vertex{pos0, sf::Color::Green};
   _sprite[1] = sf::Vertex{pos1, sf::Color::Green};
   _sprite[2] = sf::Vertex{pos2, sf::Color::Green};
   _sprite[3] = sf::Vertex{pos3, sf::Color::Green};
-  _sprite[4] = sf::Vertex{pos4, sf::Color::Green};
-  this->setOrigin(10.0f, 10.0f);
+  _sprite[4] = sf::Vertex{pos0, sf::Color::Green};
   this->setPosition(sf::Vector2f{500.0f, 500.0f});
 }
 
