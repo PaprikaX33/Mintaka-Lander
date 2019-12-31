fd7ee3ceeeca1ea19edc7a7fbf5d94cf4021bfba
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-12-31 23:09:48 +0700

    moved the vectoring in player

--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <SFML/Graphics/RenderTarget.hpp>
+#include "utils/Vectors.hpp"
 #include "Player.hpp"
 
 static constexpr std::array<std::size_t, 3> important{0,1,3};
@@ -8,11 +9,16 @@ Player::Player(void) :
   _velocity{0.0f, 0.0f},
   _sprite{sf::LineStrip, 5}
 {
-  _sprite[0] = sf::Vertex{sf::Vector2f{10.0f,  0.0f}, sf::Color::Green};
-  _sprite[1] = sf::Vertex{sf::Vector2f{20.0f, 20.0f}, sf::Color::Green};
-  _sprite[2] = sf::Vertex{sf::Vector2f{10.0f, 15.0f}, sf::Color::Green};
-  _sprite[3] = sf::Vertex{sf::Vector2f{ 0.0f, 20.0f}, sf::Color::Green};
-  _sprite[4] = sf::Vertex{sf::Vector2f{10.0f,  0.0f}, sf::Color::Green};
+  auto const pos0 = sf::Vector2f{10.0f,  0.0f};
+  auto const pos1 = sf::Vector2f{20.0f, 20.0f};
+  auto const pos2 = sf::Vector2f{10.0f, 15.0f};
+  auto const pos3 = sf::Vector2f{ 0.0f, 20.0f};
+  auto const pos4 = sf::Vector2f{10.0f,  0.0f};
+  _sprite[0] = sf::Vertex{pos0, sf::Color::Green};
+  _sprite[1] = sf::Vertex{pos1, sf::Color::Green};
+  _sprite[2] = sf::Vertex{pos2, sf::Color::Green};
+  _sprite[3] = sf::Vertex{pos3, sf::Color::Green};
+  _sprite[4] = sf::Vertex{pos4, sf::Color::Green};
   this->setOrigin(10.0f, 10.0f);
   this->setPosition(sf::Vector2f{500.0f, 500.0f});
 }
@@ -99,7 +105,7 @@ void Player::vertical_stop(void)
     _velocity.x -= 0.1f;
   }
 }
-sf::Vector2f Player::axis_projection(sf::Vector2f const & axis)
+utls::Range<float> Player::axis_projection(sf::Vector2f const & axis) const
 {
   (void)axis;
   return {1.1f, 2.2f};
