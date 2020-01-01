54a239f57a4b252a82b27d4e67bb48616f71152a
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-01-01 18:46:52 +0700

    added single value range, and append functionalities

--- src/utils/Range.hpp
+++ src/utils/Range.hpp
@@ -9,6 +9,10 @@ namespace utls
   {
     T min;
     T max;
+    explicit constexpr Range(T const & v) :
+      min{v},
+      max{v}
+    {}
     constexpr Range(T const & l, T const & r) :
       min{std::min(l, r)},
       max{std::max(l, r)}
@@ -18,7 +22,23 @@ namespace utls
       min{std::min(l, r, comp)},
       max{std::max(l, r, comp)}
     {}
+
+    constexpr Range & append(T const & v)
+    {
+      min = std::min(min, v);
+      max = std::max(max, v);
+      return *this;
+    }
+
+    template <typename Compare>
+    constexpr Range & append(T const & v, Compare comp)
+    {
+      min = std::min(min, v, comp);
+      max = std::max(max, v, comp);
+      return *this;
+    }
   };
+
   template<typename T>
   bool overlap(T const & lf, T const & rg) noexcept
   {
