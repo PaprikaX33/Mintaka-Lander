1287ae388b36d6ef79f0aa3f216da72fba51bfc2
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-01-02 01:27:02 +0700

    moved the definition of range to a separate file

--- src/utils/Range.hpp
+++ src/utils/Range.hpp
@@ -9,41 +9,22 @@ namespace utls
   {
     T min;
     T max;
-    explicit constexpr Range(T const & v) :
-      min{v},
-      max{v}
-    {}
-    constexpr Range(T const & l, T const & r) :
-      min{std::min(l, r)},
-      max{std::max(l, r)}
-    {}
-    template <typename Compare>
-    constexpr Range(T const & l, T const & r, Compare comp) :
-      min{std::min(l, r, comp)},
-      max{std::max(l, r, comp)}
-    {}
-
-    constexpr Range & append(T const & v)
-    {
-      min = std::min(min, v);
-      max = std::max(max, v);
-      return *this;
-    }
+    explicit constexpr Range(T const & v);
+    constexpr Range(T const & l, T const & r);
 
     template <typename Compare>
-    constexpr Range & append(T const & v, Compare comp)
-    {
-      min = std::min(min, v, comp);
-      max = std::max(max, v, comp);
-      return *this;
-    }
+    constexpr Range(T const & l, T const & r, Compare comp);
+
+    constexpr Range & append(T const & v);
+
+    template <typename Compare>
+    constexpr Range & append(T const & v, Compare comp);
   };
 
   template<typename T>
-  bool overlap(T const & lf, T const & rg) noexcept
-  {
-    return (lf.min >= rg.min && lf.min <= rg.max) || (lf.max >= rg.min && lf.max <= rg.max);
-  }
+  constexpr bool overlap(Range<T> const & lf, Range<T> const & rg) noexcept;
 }
+
+#include "Range.inl"
 
 #endif //MINTAKA_UTILS_RANGE_HEAD_HPP
