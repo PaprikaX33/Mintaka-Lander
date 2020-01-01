bfeae9729af57015e86eb069ce4551a61472adc6
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-01-02 02:25:50 +0700

    completed the ground check based on the SAT

--- src/ground.cpp
+++ src/ground.cpp
@@ -1,14 +1,31 @@
+#include <iostream>
+#include <utility>
 #include <cmath>
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
 #include "Ground.hpp"
+#include "utils/Vectors.hpp"
 
 Ground::Ground(std::vector<size_t> const & in) :
   _sprite{sf::LineStrip, 11},
-  _anchor{sf::Transform::Identity}
+  _anchor{sf::Transform::Identity},
+  _heightMap{in}
+{
+  generate_sprite();
+}
+
+Ground::Ground(std::vector<size_t> && in) :
+  _sprite{sf::LineStrip, 11},
+  _anchor{sf::Transform::Identity},
+  _heightMap{std::move(in)}
+{
+  generate_sprite();
+}
+
+void Ground::generate_sprite(void)
 {
   std::size_t count = 0;
-  for(auto const & i : in){
+  for(auto const & i : _heightMap){
     _sprite[count] = sf::Vertex{sf::Vector2f{static_cast<float>(count) * 100.0f,
                                              static_cast<float>(i) * Ground::unit_height}, sf::Color::Green};
     if(count >= 11u){
@@ -23,11 +40,7 @@ Ground::Ground(std::vector<size_t> const & in) :
   _anchor.translate(0.0f, 1000.0f);
   _anchor.scale(1.0f,-1.0f);
 }
-
-Ground::~Ground(void)
-{
-
-}
+Ground::~Ground(void) = default;
 
 void Ground::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
@@ -35,21 +48,39 @@ void Ground::draw(sf::RenderTarget & target, sf::RenderStates states) const
   target.draw(_sprite, states);
 }
 
-float Ground::objRelHeight(sf::Vector2f const & pos) const
+bool Ground::is_hit(Collidable const & col) const
 {
-  if(pos.x <= 0.0f){
-    return ((_anchor * _sprite[0].position).y) - pos.y;
+  auto const imp = col.important_x();
+  int const lf = static_cast<int>(std::floor(imp.min / 100.0f));
+  int const rg = static_cast<int>(std::ceil(imp.max / 100.0f));
+  std::cout << imp.min << '\t' << imp.max << '\n';
+  std::cout << lf << '\t' << rg << '\n';
+  int step = lf;
+  while(step < rg){
+    auto const & loc_lf = step < 0 ?
+                                 _sprite[0].position :
+                                 step > static_cast<int>(_sprite.getVertexCount()) ?
+      _sprite[_sprite.getVertexCount() -1].position :
+      _sprite[static_cast<std::size_t>(step)].position;
+
+    auto const & loc_rg = step + 1 < 0 ?
+                                 _sprite[0].position :
+                                 step + 1 > static_cast<int>(_sprite.getVertexCount()) ?
+      _sprite[_sprite.getVertexCount() -1].position :
+      _sprite[static_cast<std::size_t>(step + 1)].position;
+    auto const difference = ((_anchor * loc_rg) - ( _anchor * loc_lf));
+    auto const axis = utls::normalize(utls::normal_left(difference));
+    auto const player_proj = col.axis_projection(axis);
+    auto const self_proj = utls::projection(_anchor * loc_lf,  axis);
+    std::cout << "diff " << difference.x << ',' << difference.y << '\n';
+    std::cout << "axis" << axis.x << ',' << axis.y << '\n';
+    for(auto const & i : player_proj) {
+      std::cout << "RNG: " << i.min << ',' << i.max << "\tself: " << self_proj << '\n';
+      if(i.contains(self_proj)){
+        return true;
+      }
+    }
+    step++;
   }
-  if(pos.x >= 1000.0f){
-    return ((_anchor * _sprite[10].position).y) - pos.y;
-  }
-  float const x_rel = pos.x / 100.0f;
-  std::size_t const left_bound = static_cast<std::size_t>(std::floor(x_rel));
-  float const x_diff = pos.x - (static_cast<float>(left_bound) * 100.0f);
-  float const lower = (_sprite[left_bound].position).y;
-  float const higher = (_sprite[left_bound + 1].position).y;
-  float const gradient = (higher - lower) / 100.0f;
-  float const y_dir = (gradient * x_diff) + lower;
-  sf::Vector2f const groundPoint = _anchor * sf::Vector2f{pos.x, y_dir};
-  return groundPoint.y - pos.y;
+  return false;
 }
--- src/include/Ground.hpp
+++ src/include/Ground.hpp
@@ -6,21 +6,24 @@
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Transform.hpp>
 #include <SFML/Graphics/VertexArray.hpp>
+#include "Collidable.hpp"
 
 class Ground final : public sf::Drawable
 {
 private:
   sf::VertexArray _sprite;
   sf::Transform _anchor;
+  std::vector<std::size_t> _heightMap;
 public:
   static constexpr float unit_height = 50.0f;
 private:
   virtual void draw(sf::RenderTarget &, sf::RenderStates) const override;
+  void generate_sprite(void);
 public:
   Ground(std::vector<std::size_t> const &);
+  Ground(std::vector<std::size_t> &&);
   virtual ~Ground(void) override;
-  float objRelHeight(sf::Vector2f const & pos) const;
-
+  bool is_hit(Collidable const &) const;
   static Ground generate_file(char const *);
 };
 
