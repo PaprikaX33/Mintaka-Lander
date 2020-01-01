8b3c43063d06408115a9bbebf87992d2fdaf34a0
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-01-02 02:27:04 +0700

    removed the unused collision file

--- src/include/Collision.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MINTAKA_COLLISION_DETECTION_HEAD_HPP
-#define MINTAKA_COLLISION_DETECTION_HEAD_HPP
-#include "Collidable.hpp"
-
-bool collide(Collidable const &, Collidable const &) noexcept;
-
-#endif //MINTAKA_COLLISION_DETECTION_HEAD_HPP
