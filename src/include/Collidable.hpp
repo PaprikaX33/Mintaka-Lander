e72d29fb5a9dca715aed830862eefb4f02612bad
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-01-02 22:04:36 +0700

    restructured the collidable

--- src/include/Collidable.hpp
+++ src/include/Collidable.hpp
@@ -6,8 +6,9 @@
 
 struct Collidable
 {
-  virtual utls::Range<float> important_x(void) const = 0;
-  virtual std::vector<utls::Range<float>> axis_projection(sf::Vector2f const & axis) const = 0;
+  virtual std::size_t number_of_object(void) const = 0;
+  virtual utls::Range<float> important_x(std::size_t obj_no) const = 0;
+  virtual utls::Range<float> axis_projection(sf::Vector2f const & axis, std::size_t obj_no) const = 0;
   virtual ~Collidable() = default;
 };
 
